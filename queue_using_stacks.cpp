a9fcbdae156241145486a8b84c03d247051f9e12
Hiral Jain <71397276+HiralJain112@users.noreply.github.com>
2021-05-30 23:39:41 +0530

    Add files via upload

---
+++ queue_using_stacks.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Queue
+{
+    stack<int> s1;
+    stack<int> s2;
+
+    public:
+       void push(int x)
+       {
+           s1.push(x);
+       }
+       int pop()
+       {
+           if (s1.empty() and s2.empty())
+           {
+               cout<<"Queue is empty\n";
+               return -1;
+           }
+           if (s2.empty())
+           {
+               while (!s1.empty())
+               {
+                   s2.push(s1.top());
+                   s1.pop();
+               }
+            }   
+            int topval = s2.top();
+            s2.pop();
+            return topval; 
+       }
+       bool empty()
+       {
+            if (s1.empty() and s2.empty())
+            {
+               return true;
+            }
+            return false;
+        }   
+};
+int main()
+{
+    Queue q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(5);
+    cout<<q.pop()<<endl;
+    q.push(6);
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    return 0;
+}
