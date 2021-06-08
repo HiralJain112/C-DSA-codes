4758926fa3fdd77f6856833ff3ce94bdf38f6aa2
Hiral Jain <71397276+HiralJain112@users.noreply.github.com>
2021-06-08 21:47:44 +0530

    Add files via upload

---
+++ string hashing.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+int p = 31;
+const int N = 1e5+3, m = 1e9 +7;
+vector<long long> powers(N);
+
+long long calculate_hash(string s)
+{
+    long long hash = 0;
+    for (int i = 0; i <= s.size(); i++)
+    {
+        hash = (hash + (s[i] -'a' +1)*powers[i])%m;
+    }
+    return hash;
+}
+
+int main()
+{
+    powers[0] = 1;
+    for (int i = 1; i < N; i++)
+    {
+        powers[i] = (powers[i-1]*p)%m;
+    }
+    
+    vector<string> str = {"aa","ab","aa","b","cc","aa"};
+    vector<long long> hashes;
+    for (auto w : str)
+    {
+       hashes.push_back(calculate_hash(w));
+    }
+    //O(nm)
+    sort(hashes.begin(),hashes.end());
+    //O(nlog n)
+    //total time complexity = O(nm + nlogn)
+    int distinct = 0;
+    for (int i = 0; i < hashes.size(); i++)
+    {
+        if (i==0 or hashes[i]!=hashes[i-1])
+        {
+           distinct++;
+        }
+    }
+    cout<<distinct<<endl;
+    return 0;
+}
